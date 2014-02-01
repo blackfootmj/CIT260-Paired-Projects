33485080428e7de65f62ce8e2e88f5ae57cd688f
Michael Johnson <blackfootmj@gmail.com>
2014-02-01 12:03:47 -0700

    Added Winner.java

---
+++ checkers/src/checkers/Winner.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package checkers;
+
+import java.util.Scanner;
+ 
+public class Winner {
+    int won = 6;
+    int lost = 12;
+    int total = 0;
+    
+    Scanner input = new Scanner(System.in);
+    private int Win;
+    private int Loss;
+
+    public int winner (){
+	if (won > 11){
+	    System.out.println("You Win");
+	    return Win;
+	}
+	if (lost > 11 ){
+	    System.out.println("You Lose");
+	    return Loss;
+        }
+  
+   
+	total = ((won * 100) + (lost * 50));
+
+         return total; 
+    }
+    
+    
+    
+}
