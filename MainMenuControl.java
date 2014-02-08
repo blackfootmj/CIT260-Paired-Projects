65dafa09959daa79e95b94787199d2c0c2632e53
Michael Johnson <blackfootmj@gmail.com>
2014-02-08 13:09:40 -0700

    Main Menu - Individual Programming

---
+++ MainMenuControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chkrs;
+
+/**
+ *
+ * @author michael
+ */
+public class MainMenuControl {
+ 
+    public void instructions(){
+	
+
+
+    }
+    
+    public void navigation(){
+	
+    }
+    
+    public void general(){
+    
+	
+    }
+}
